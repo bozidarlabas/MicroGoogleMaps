a0ec9fbb9e5548e8e16c4ebe92dba4dea8ad10ea
bozidar <bozidar.labas@hotmail.com>
2015-11-02 21:21:51 +0100

    Creating the API Client

--- app/src/main/java/com/bozidar/labas/googlemaps/fragments/MapFragment.java
+++ app/src/main/java/com/bozidar/labas/googlemaps/fragments/MapFragment.java
@@ -1,9 +1,12 @@
 package com.bozidar.labas.googlemaps.fragments;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -17,7 +20,49 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
         GoogleMap.OnInfoWindowClickListener,
         GoogleMap.OnMapLongClickListener,
         GoogleMap.OnMapClickListener,
-        GoogleMap.OnMarkerClickListener{
+        GoogleMap.OnMarkerClickListener {
+
+    //Getting the user's location for initializing the map camera
+    private GoogleApiClient googleApiClient;
+    private Location currentLocation;
+
+    //Switching between different map display types
+    private final int[] MAP_TYPES = {
+            GoogleMap.MAP_TYPE_NORMAL,
+            GoogleMap.MAP_TYPE_HYBRID,
+            GoogleMap.MAP_TYPE_TERRAIN,
+            GoogleMap.MAP_TYPE_NONE};
+    private int curMapTypeIndex;
+
+    /**
+     * Creating GoogleApiClient and intializatin of LocationServices in order to get users current location
+     * @param view
+     * @param savedInstanceState
+     */
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setHasOptionsMenu(true);
+
+        googleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        initListeners();
+    }
+
+    /**
+     * The initListeners method binds the interfaces
+     */
+    private void initListeners() {
+        getMap().setOnMarkerClickListener(this);
+        getMap().setOnMapLongClickListener(this);
+        getMap().setOnInfoWindowClickListener( this );
+        getMap().setOnMapClickListener(this);
+    }
 
     @Override
     public void onConnected(Bundle bundle) {
